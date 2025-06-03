d9f33b58d696a0659470e63282bf9bf963c86f4b
shweta roy <128317014+ShwetaRoy17@users.noreply.github.com>
2025-06-03 13:42:26 +0530

    dfa

--- src/main/java/com/app/FinTrack/FinTrackApplication.java
+++ src/main/java/com/app/FinTrack/FinTrackApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinTrackApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Project started");
 		SpringApplication.run(FinTrackApplication.class, args);
 	}
 
